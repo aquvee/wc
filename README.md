526cd2b29d1a3ca611082d866d36bd9298061571
Kodai Aoyama <kodai.aoyama+github@gmail.com>
2024-01-07 20:22:27 +0900

    fix

--- README.md
+++ README.md
@@ -1,54 +1,56 @@
 # Aquvee WebComponent
 
-## Usage
+## Introduction
 
-### HTML Integration
+The `AquveeComponent` is a versatile and customizable web component designed to easily integrate dynamic content into your web pages. It supports flexible configuration through attributes and enables seamless data fetching and display.
 
-The `aquvee-component` is a custom web component designed for easy embedding in your HTML pages with your data. Here's a step-by-step guide on how to implement it:
+## Installation
 
-#### Basic Setup
-
-Start by adding the required script to your HTML file. Place the script tag just before the `</body>` tag to ensure proper loading:
+To use the `AquveeComponent`, first include the script in your HTML file. It's recommended to place the script tag just before the closing `</body>` tag for optimal loading:
 
 ```html
 <script src="https://cdn.jsdelivr.net/gh/aquvee/wc@latest/index.js"></script>
 ```
 
-#### Implementing the Component
+## Usage
 
-To incorporate the `aquvee-component` into your webpage, insert it within the `<body>` section. Below is an illustrative example:
+### Basic Integration
+
+Insert the `aquvee-component` into your webpage within the `<body>` section, like this:
 
 ```html
-<aquvee-component query="Please tell me five pieces of user information"
-                aquvee_url="http://localhost:8000/wc">
-    table
+<aquvee-component query="Your query here" is-dev project-id="your-project-id">
+    <!-- Your desired format goes here -->
 </aquvee-component>
 ```
 
-##### Attributes
+### Attributes
 
-- `query`: This is the query string used for data retrieval. In this example, it's `"Please tell me five pieces of user information"`.
+- `query`: The query string used for data retrieval.
+- `is-dev`: (Optional) Include this attribute for development environment configurations. This modifies the request URL to a development server.
+- `project-id`: (Optional) Your unique project identifier. Used in constructing the request URL for the production server.
+- `style-css`: (Optional) This attribute allows you to define custom CSS styles for the AquveeComponent. The specified styles will be applied within the component's Shadow DOM.
+- `custom-url`: (Optional) Custom URL for specific endpoint configurations.
 
-- `aquvee_url`: The endpoint URL where the component fetches data. Here, it's set to `http://localhost:8000/wc`.
+### Child Content
 
-##### Child Content
+Define the output format directly as child content of the component. It can be a simple keyword like `"table"` or a more complex HTML structure, allowing for customized display styles.
 
-Specify the output format. It can be a keyword like `"table"` or a custom HTML structure, e.g., `<div class="card-wrapper"><div class="card"></div></div>`. This flexibility allows for varied display styles.
+### Example
 
-#### Complete Example
-
-Here's the complete setup showcasing how to embed the `aquvee-component` in an HTML document:
+Here's a complete example illustrating the integration of the `AquveeComponent` in an HTML document:
 
 ```html
 <!DOCTYPE html>
 <html>
 <head></head>
 <body>
-    <aquvee-component query="Please tell me five pieces of user information"
-                    aquvee_url="http://localhost:8000/wc">
+    <aquvee-component query="Please tell me five pieces of user information" is-dev project-id="your-project-id">
         table
     </aquvee-component>
     <script src="https://cdn.jsdelivr.net/gh/aquvee/wc@latest/index.js"></script>
 </body>
 </html>
 ```
+
+This component dynamically fetches and displays data based on the specified query and configuration attributes. It provides a flexible and efficient way to integrate data-driven content into your web pages.
--- index.js
+++ index.js
@@ -1,50 +1,80 @@
+/**
+ * デフォルトのローダースタイルをシャドウDOMに追加する。
+ * @param {ShadowRoot} shadowRoot - このコンポーネントのシャドウルート。
+ */
+function addDefaultStyle(shadowRoot) {
+    const style = document.createElement('style');
+    style.textContent = `
+        // ローダーの基本スタイルを定義
+        .loader {
+            margin: 0 auto;
+            width: 50px;
+            aspect-ratio: 1;
+            display: grid;
+        }
+        .loader::before,
+        .loader::after {    
+            content:"";
+            grid-area: 1/1;
+            --c:no-repeat radial-gradient(farthest-side,#25b09b 92%,#0000);
+            background: 
+                var(--c) 50%  0, 
+                var(--c) 50%  100%, 
+                var(--c) 100% 50%, 
+                var(--c) 0    50%;
+            background-size: 12px 12px;
+            animation: l12 1s infinite;
+        }
+        .loader::before {
+            margin: 4px;
+            filter: hue-rotate(45deg);
+            background-size: 8px 8px;
+            animation-timing-function: linear
+        }
+        @keyframes l12 { 
+            100%{transform: rotate(.5turn)}
+        }
+    `;
+    shadowRoot.appendChild(style);
+}
+
+/**
+ * カスタムスタイルタグをシャドウDOMに追加し、将来の更新のために参照を保持する。
+ * @param {ShadowRoot} shadowRoot - このコンポーネントのシャドウルート。
+ * @returns {HTMLStyleElement} - 追加されたカスタムスタイルタグ。
+ */
+function addCustomStyleTag(shadowRoot) {
+    const customStyleTag = document.createElement('style');
+    shadowRoot.appendChild(customStyleTag);
+    return customStyleTag;
+}
+
 class AquveeComponent extends HTMLElement {
+    /**
+     * AquveeComponent のコンストラクタ。
+     * シャドウルートの設定とスタイルの初期化を行う。
+     */
     constructor() {
         super();
         this._initialized = false;
         this._observer = null;
-        this._css = `
-            .loader {
-                margin: 0 auto;
-                width: 50px;
-                aspect-ratio: 1;
-                display: grid;
-            }
-            .loader::before,
-            .loader::after {    
-                content:"";
-                grid-area: 1/1;
-                --c:no-repeat radial-gradient(farthest-side,#25b09b 92%,#0000);
-                background: 
-                    var(--c) 50%  0, 
-                    var(--c) 50%  100%, 
-                    var(--c) 100% 50%, 
-                    var(--c) 0    50%;
-                background-size: 12px 12px;
-                animation: l12 1s infinite;
-            }
-            .loader::before {
-                margin: 4px;
-                filter: hue-rotate(45deg);
-                background-size: 8px 8px;
-                animation-timing-function: linear
-            }
-            @keyframes l12 { 
-                100%{transform: rotate(.5turn)}
-            }
-        `;
-        this._format = (()=>{
-            const innerHTMLContent = this.innerHTML.trim();
-            if (innerHTMLContent) {
-                return innerHTMLContent; 
-            } else {
-                return "div"; 
-            }
-        })();
+        this._format = null;
+        // シャドウルートを作成
+        this.shadowRoot = this.attachShadow({mode: 'open'});
+        // スタイルの設定
+        addDefaultStyle(this.shadowRoot);
+        this.customStyleTag = addCustomStyleTag(this.shadowRoot);
+        // コンテンツ用のコンテナを作成
+        this.contentContainer = document.createElement('div');
+        this.shadowRoot.appendChild(this.contentContainer);
     }
 
+    /**
+     * Intersection Observer を初期化し、要素がビューポートに入るのを監視する。
+     */
     initIntersectionObserver() {
         if(this._observer) return;
+        // Intersection Observerの設定
         this._observer = new IntersectionObserver((entries, observer) => {
             // 要素が画面内に入った場合にのみfetchを実行
             entries.forEach(entry => {
@@ -55,45 +85,89 @@ class AquveeComponent extends HTMLElement {
                 }
             });
         }, { threshold: 0.1 }); // 要素が10%画面に入った時に発火
-
-        this._observer.observe(this); // 要素の観察を開始
+        // 要素の観察を開始
+        this._observer.observe(this);
     }
 
+    /**
+     * カスタムエレメントがDOMに接続された際に呼ばれるコールバック。
+     * 必要な設定の初期化を行う。
+     */
     connectedCallback() {
+        // formatの設定
+        this._format = this.innerHTML.trim() || "div";
+        // style-cssの設定
+        const styleCss = this.getAttribute('style-css');
+        if (styleCss) {
+            this.customStyleTag.textContent = styleCss;
+        }
         // Intersection Observerの設定と初期化
         this.initIntersectionObserver();
     }
 
+    /**
+     * 監視する属性のリストを返す。
+     * @returns {string[]} - 監視する属性名の配列。
+     */
     static get observedAttributes() {
-        return ['query', 'aquvee_url'];
+        return ['style-css', 'query', 'is-dev', 'project-id', 'custom-url'];
     }
 
-    attributeChangedCallback(_, oldValue, newValue) {
-        // 属性が変更された場合にのみ実行
-        if (oldValue !== newValue) {
-            if (this._initialized) {  // 初期化後のみ再レンダリング
-                this.render();
-            }
+    /**
+     * 属性が変更された際に呼ばれるコールバック。
+     * @param {string} name - 変更された属性の名前。
+     * @param {string} oldValue - 古い属性値。
+     * @param {string} newValue - 新しい属性値。
+     */
+    attributeChangedCallback(name, _, newValue) {
+        if (name === 'style-css') {
+            this.customStyleTag.textContent = newValue;
+        } else if (this._initialized) {
+            // style-css以外の属性が変更された場合にのみ再レンダリング
+            this.render();
         }
     }
 
+    /**
+     * コンポーネントの主要なレンダリングロジック。
+     * サーバーからデータを取得し、DOMに内容を表示する。
+     */
     async render() {
-        const format = this._format
+        const format = this._format;
         const query = this.getAttribute('query');
-        const aquvee_url = this.getAttribute('aquvee_url');
         const url = window.location.href;
 
-        // query または aquvee_url が未設定ならエラー表示
-        if (!query || !aquvee_url) {
-            this.innerHTML = "Error you should set query and aquvee_url";
+        // 'is-dev' 属性の存在に基づいて URL を決定
+        const aquveeUrl = (()=>{
+            const projectId = this.getAttribute('project-id');
+            const customUrl = this.getAttribute('custom-url');
+            const isDev = this.hasAttribute('is-dev');
+            if (isDev) {
+                if (customUrl) {
+                    return `${customUrl}/dev`;
+                } else if (projectId) {
+                    return `https://example.com/${projectId}/dev`;
+                } else{
+                    return 'http://localhost:8000/dev';
+                }
+            } else if(customUrl) {
+                return customUrl;
+            } else {
+                return `https://example.com/${projectId}/wc`;
+            }
+        })();
+
+        // query が未設定ならエラー表示
+        if (!query) {
+            this.displayError("Error: you should set query");
             return;
         }
 
         // ローディング状態の表示
-        this.innerHTML = `<style>${this._css}</style><div class="aquvee"><div class="loader"></div></div>`;
+        this.displayLoading();
 
         try {
-            const response = await fetch(aquvee_url, {
+            const response = await fetch(aquveeUrl, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -101,13 +175,56 @@ class AquveeComponent extends HTMLElement {
                 body: JSON.stringify({ format, query, url })
             });
             const data = await response.json();
-            
-            // ここで取得したデータを表示
-            this.innerHTML = data.content;
+            // 取得したデータを表示
+            this.displayContent(data.content);
         } catch (error) {
             console.error('Data fetch error:', error);
-            this.innerHTML = "Error fetching data";
+            this.displayError("Error fetching data");
         }
+    }
+    
+    /**
+     * ローディングインジケーターを表示する。
+     * データ取得中や処理中にユーザーにフィードバックを提供するために使用される。
+     */
+    displayLoading() {
+        const loader = document.createElement('div');
+        loader.innerHTML = '<div class="loader"></div>';
+        this.updateDOM(loader);
+    }
+
+    /**
+     * サーバーから取得したコンテンツを表示する。
+     * @param {string} content - 表示するHTMLまたはテキストコンテンツ。
+     */
+    displayContent(content) {
+        const container = document.createElement('div');
+        container.innerHTML = content;
+        this.updateDOM(container);
+    }
+
+    /**
+     * エラーメッセージを表示する。
+     * ネットワークエラーやサーバーからの応答エラーなど、問題が発生した場合に使用される。
+     * @param {string} message - 表示するエラーメッセージ。
+     */
+    displayError(message) {
+        const errorContainer = document.createElement('div');
+        errorContainer.textContent = message;
+        this.updateDOM(errorContainer);
+    }
+
+    /**
+     * DOMを更新する汎用的なメソッド。
+     * コンテンツコンテナ内のみを更新し、スタイルは保持する。
+     * @param {HTMLElement} content - DOMに追加する要素。
+     */
+    updateDOM(content) {
+        const fragment = document.createDocumentFragment();
+        fragment.appendChild(content);
+        // コンテンツコンテナのみをクリアして更新
+        this.contentContainer.innerHTML = '';
+        this.contentContainer.appendChild(fragment);
     }
 }
 
